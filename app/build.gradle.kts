7357a447733637dcec8dde5e9b0ecf4fcfa3f9b3
JBcodes10 <justinbrownrussell@gmail.com>
2026-04-27 13:06:01 -0400

    Update: Updating Home page with 2 x 2 grid buttons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ android {
 
 dependencies {
     implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.android.material:material:1.11.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
